4a7e505217784dccbbbcf83a9d77d7b6178cdb0e
ehsan <ehsansaraee6570@yahoo.com>
2022-02-15 21:44:59 +0330

    accordion clicked logic added

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,18 +1,36 @@
 import { IconContext } from 'react-icons';
 import { data } from '../data';
 import styled from 'styled-components';
+import { useState } from 'react';
+import { FiMinus, FiPlus } from 'react-icons/fi';
 
 const Accordion = () => {
+   const [clicked, setClicked] = useState(false);
+
+   const toggle = (index) => {
+      if (clicked === index) {
+         setClicked(null);
+      }
+      setClicked(index);
+   };
+
    return (
       <IconContext.Provider value={{ color: '00ffb9', size: '25px' }}>
          <AccordionSection>
             <Container>
-               {data?.map(({ question, answer }) => (
+               {data?.map(({ question, answer }, index) => (
                   <>
-                     <Wrap>
+                     <Wrap onClick={() => toggle(index)} key={index}>
                         <h1>{question}</h1>
+                        <span>
+                           {clicked === index ? <FiMinus /> : <FiPlus />}
+                        </span>
                      </Wrap>
-                     <p>{answer}</p>
+                     {clicked === index ? (
+                        <Dropdown>
+                           <p>{answer}</p>
+                        </Dropdown>
+                     ) : null}
                   </>
                ))}
             </Container>
@@ -49,6 +67,27 @@ const Wrap = styled.div`
 
    h1 {
       padding: 2rem;
+      font-size: 2rem;
+   }
+
+   span {
+      margin-right: 1.5rem;
+   }
+`;
+
+const Dropdown = styled.div`
+   background: #1c1c1c;
+   color: #00ffb9;
+   width: 100%;
+   height: 100px;
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   border-top: 1px solid #00ffb9;
+   border-bottom: 1px solid #00ffb9;
+
+   p {
       font-size: 2rem;
    }
 `;
