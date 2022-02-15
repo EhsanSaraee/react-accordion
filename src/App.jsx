2eeb91e7b783e03b32f9b6c1670d7cf0486a7410
ehsan <ehsansaraee6570@yahoo.com>
2022-02-15 20:44:55 +0330

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Accordion from './Components/Accordion';
+
+const App = () => {
+   return <Accordion />;
+};
+
+export default App;
---
+++ src/Components/Accordion.jsx
@@ -0,0 +1,9 @@
+const Accordion = () => {
+  return ( 
+    <>
+      <h1>Accordion</h1>
+    </>
+   );
+}
+ 
+export default Accordion;
